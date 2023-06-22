04fd8509b526748d12bd4862086f032eaabaf12e
manas-singh19 <41261719+manas-singh19@users.noreply.github.com>
2023-06-22 13:45:09 +0530

    2 files modified

--- App.js
+++ App.js
@@ -2,9 +2,5 @@ import * as React from 'react';
 import { useState, useEffect, useRef } from 'react';
 
 export default function App() {
-  return (
-    <>
-      <h1>manas</h1>
-    </>
-  );
+  return <></>;
 }
