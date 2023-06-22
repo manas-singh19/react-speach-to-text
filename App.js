834c44a59ceb4065589595fb3202236d43c6ebd4
manas-singh19 <41261719+manas-singh19@users.noreply.github.com>
2023-06-22 13:39:40 +0530

    3 files added, 4 deleted

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
+// axios
+import axios from 'axios';
+
+export default function App() {
+  return (
+    <>
+      <h1>manas</h1>
+    </>
+  );
+}
